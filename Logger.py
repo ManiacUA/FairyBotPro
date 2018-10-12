07666d82f778d209d2e8cd298b799fa4dbf09417
Maniac <danylovarushchyk@gmail.com>
2018-10-12 16:52:39 +0200

    Some changes to Logger, probably those are final:)

--- Logger.py
+++ Logger.py
@@ -2,12 +2,11 @@
 What should be logged:
 1) Start at the start of a function.
 2) Finish at the end of function.
-3) Result of any action/request.
+3) Result of any calculation/request.
 
 Rules:
 1) Logs initialisation should be first step of initialisation of module;
 2) Entities should never be monitored;
-3) Modules should have add_tabs=1, functions should increase their tabs when called one inside another
 """
 
 import Tool
@@ -15,36 +14,53 @@ import datetime
 
 
 class Logger:
-    func = "(func not set)"
     tab = "  "  # for saving space purpose tab is 2 spaces
 
-    def __init__(self, module, account, add_tabs=0, show_logs=True, show_warnings=True):
-        self.add_tabs = add_tabs
+    def __init__(self, module, account, show_logs=True, show_warnings=True):
         self.module = module
         self.show_logs = show_logs
         self.show_warnings = show_warnings
-        self.log(account, "Logger initialised in this module.")
+        self.email = account.email
+        self.depth = 0
+        self.func = None
+        self.log_func_start('__init__')
+        self.log_func_finish()
 
-    def set_func(self, func):
-        """Sets func in message to this value. Custom parameter in message will use custom but won't override it."""
-        self.func = func
+    def check_func(self):
+        if not self.func:
+            raise ValueError("Function value in logger is not set.")
 
-    def log(self, account, text, func=None, tabs=0):
+    def log(self, text):
         if self.show_logs:
-            Tool.Sprint("... " + self._message(self, account, text, func, tabs))
+            self._message(text=text, pretext='...')
 
-    def warning(self, account, text, func=None, tabs=0):
+    def log_func_start(self, func):
+        self.depth += 1
+        if self.show_logs:
+            self.log("Started.")
+
+    def log_func_finish(self):
+        if self.show_logs:
+            self.log("Finished.")
+        self.depth -= 1
+        self.func = None
+
+    def warning(self, text):
         if self.show_warnings:
-            Tool.Sprint("!!! " + self._message(self, account, text, func, tabs))
+            self._message(text=text, pretext='!!!')
 
-    def error(self, account, text, func=None, tabs=0):
-        Tool.Sprint("ERR " + self._message(self, account, text, func, tabs))
-        exit()  # TODO: check if that thing will stop thread or whole script or probably just throw exception
+    def error(self, text):
+        self._message(text=text, pretext='<ERROR>')
+        exit()  # TODO: check if that thing will stop thread or whole script, need to stop thread only
 
-    def _message(self, account, text, func, tabs):
+    def _message(self, text, pretext=""):
+        self.check_func()
         time_string = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-        if func is None:
-            func = self.func
-        text = "{}[{}] {}{}({}): {}".format(
-            time_string, account.email, (self.add_tabs + tabs) * self.tab, self.module, func, text)
+        text = "{}{}[{}] {}{}({}): {}".format(pretext,
+                                              time_string,
+                                              self.email,
+                                              self.depth * self.tab,
+                                              self.module,
+                                              self.func,
+                                              text)
         return text
